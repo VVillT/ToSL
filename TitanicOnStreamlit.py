1d69eec316cf1342b567d1a2444dd110f457cbf2
VVillT <whtongwilliam@gmail.com>
2024-10-24 19:42:33 +0100

    Update wth simpler languages for god damn Streamlit

--- TitanicOnStreamlit.py
+++ TitanicOnStreamlit.py
@@ -12,17 +12,16 @@ st.subheader('Making Prediction')
 st.markdown('**Please provide information of the passenger and we will see how likely he will survive**:')  # you can use markdown like this
 
 # load models
-tree_clf = joblib.load('gbc_best.pickle')
+tree_clf = joblib.load('rf_optimal.pickle')
 
 # get inputs
 
 sex = st.selectbox('Sex', ['female', 'male'])
 age = int(st.number_input('Age:', 0, 120, 20))
 sib_sp = int(st.number_input('# of siblings / spouses aboard:', 0, 10, 0))
-#par_ch = int(st.number_input('# of parents / children aboard:', 0, 10, 0))
+par_ch = int(st.number_input('# of parents / children aboard:', 0, 10, 0))
 pclass = st.selectbox('Ticket class (1 = 1st, 2 = 2nd, 3 = 3rd)', [1, 2, 3])
-fare = int(st.number_input('# of parents / children aboard:', 0, 100, 0))
-#embarked = st.selectbox('Port of Embarkation (C = Cherbourg, Q = Queenstown, S = Southampton)', ['C', 'Q', 'S'])
+embarked = st.selectbox('Port of Embarkation (C = Cherbourg, Q = Queenstown, S = Southampton)', ['C', 'Q', 'S'])
 
 passenger = pd.DataFrame(
     {
@@ -30,17 +29,41 @@ passenger = pd.DataFrame(
         'Sex': [sex], 
         'Age': [age],
         'SibSp': [sib_sp],
-#        'Parch': [par_ch],
-        'Fare': [fare],
-#        'Embarked': [embarked],
+        'Parch': [par_ch],
+#        'Fare': [fare],
+        'Embarked': [embarked],
     }
 )
 
-y_pred = tree_clf.predict(passenger)
+def transform_passenger_data(passenger_data):
+    # Initialize transformed data as a dictionary
+    transformed_data = {
+        'Age': passenger_data['Age'][0] if passenger_data['Age'] is not None else np.nan,
+        'SibSp': passenger_data['SibSp'][0],
+        'Parch': passenger_data['Parch'][0],
+        'Sex_male': 1 if passenger_data['Sex'][0] == 'male' else 0,
+        'Embarked_Q': 1 if passenger_data['Embarked'][0] == 'Q' else 0,
+        'Embarked_S': 1 if passenger_data['Embarked'][0] == 'S' else 0,
+        'Pclass_2': 1 if passenger_data['Pclass'][0] == 2 else 0,
+        'Pclass_3': 1 if passenger_data['Pclass'][0] == 3 else 0
+    }
+    
+    # Convert the dictionary to a DataFrame with the correct column order
+    df_transformed = pd.DataFrame([transformed_data])
+    
+    return df_transformed
+
+transformed_passenger = transform_passenger_data(passenger)
+transformed_passenger
+
+
+
+y_pred = tree_clf.predict(transformed_passenger)
+predtest = tree_clf.predict_proba(transformed_passenger)
 
 if y_pred[0] == 0:
-    msg = 'This passenger is predicted to be: **died**'
+    msg = 'This passenger is predicted to have {:.2f} chance to be: **died**'.format(predtest[0]*100)
 else:
-    msg = 'This passenger is predicted to be: **survived**'
+    msg = 'This passenger is predicted to have {:.2f} chance to be:be: **survived**'.format(predtest[1]*100)
 
 st.write(msg)
